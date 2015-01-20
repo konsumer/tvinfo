585a99c180e9574d5b54084a836e57591e852e25
David Konsumer <konsumer@jetboystudio.com>
2015-01-20 03:15:10 -0800

    bump to v0.1.23

--- test/tvinfo.test.js
+++ test/tvinfo.test.js
@@ -6,7 +6,6 @@ describe('tvinfo', function(){
   describe('.shows()', function(){
     it('should get full list of shows', function(done){
       tvinfo.shows().then(function(res){
-        console.log(res);
         expect(res).to.be.ok();
         expect(res.length).to.be.greaterThan(1000);
         done();
