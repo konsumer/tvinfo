3d4d4b35cc2ebf4276d7c9f014cf700ec3fbde63
David Konsumer <konsumer@jetboystudio.com>
2015-01-19 18:19:33 -0800

    bumper

---
+++ bump.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var pkg = require('./package.json');
+var exec = require('child_process').exec;
+
+var v = pkg.version.split('.');
+v[2]++;
+pkg.version = v.join('.');
+
+fs.writeFile('package.json', JSON.stringify(pkg,null,2), function(err){
+  if (err){
+    console.error(err);
+    process.exit(1);
+  }
+  exec('git stash && git tag v' + pkg.version + '&& git push --tags && git push', function(err, stdout, stderr){
+    if (err){
+      console.error(err);
+      process.exit(1);
+    }
+    console.log('bumped to v' + pkg.version);
+  });
+});
